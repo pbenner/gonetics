7adaeb349835a7dfb90527ddafd4649b154f6c7a
Philipp Benner <benner@molgen.mpg.de>
2019-03-28 13:22:40 +0100

    2019/03/28-13:22:40 (Linux sirius unknown)

--- alphabet.go
+++ alphabet.go
@@ -197,3 +197,166 @@ func (GappedNucleotideAlphabet) Complement(i byte) (byte, error) {
 func (GappedNucleotideAlphabet) IsWildcard(i byte) bool {
   return i == 'n' || i == 'N'
 }
+
+/* -------------------------------------------------------------------------- */
+
+type IupacNucleotideAlphabet struct {
+}
+
+func (IupacNucleotideAlphabet) Bases(i byte) ([]byte, error) {
+  switch i {
+  case 'A': fallthrough
+  case 'a': return []byte{'a'}, nil
+  case 'C': fallthrough
+  case 'c': return []byte{'c'}, nil
+  case 'G': fallthrough
+  case 'g': return []byte{'g'}, nil
+  case 'T': fallthrough
+  case 't': return []byte{'t'}, nil
+  case 'W': fallthrough
+  case 'w': return []byte{'a', 't'}, nil
+  case 'S': fallthrough
+  case 's': return []byte{'c', 'g'}, nil
+  case 'M': fallthrough
+  case 'm': return []byte{'a', 'c'}, nil
+  case 'K': fallthrough
+  case 'k': return []byte{'g', 't'}, nil
+  case 'R': fallthrough
+  case 'r': return []byte{'a', 'g'}, nil
+  case 'Y': fallthrough
+  case 'y': return []byte{'c', 't'}, nil
+  case 'B': fallthrough
+  case 'b': return []byte{'c', 'g', 't'}, nil
+  case 'D': fallthrough
+  case 'd': return []byte{'a', 'g', 't'}, nil
+  case 'H': fallthrough
+  case 'h': return []byte{'a', 'c', 't'}, nil
+  case 'V': fallthrough
+  case 'v': return []byte{'a', 'c', 'g'}, nil
+  case 'N': fallthrough
+  case 'n': return []byte{'a', 'c', 'g', 't'}, nil
+  default:  return nil, fmt.Errorf("Code(): `%c' is not part of the alphabet", i)
+  }
+}
+
+func (IupacNucleotideAlphabet) Code(i byte) (byte, error) {
+  switch i {
+  case 'A': fallthrough
+  case 'a': return 0, nil
+  case 'C': fallthrough
+  case 'c': return 1, nil
+  case 'G': fallthrough
+  case 'g': return 2, nil
+  case 'T': fallthrough
+  case 't': return 3, nil
+  case 'W': fallthrough
+  case 'w': return 4, nil
+  case 'S': fallthrough
+  case 's': return 5, nil
+  case 'M': fallthrough
+  case 'm': return 6, nil
+  case 'K': fallthrough
+  case 'k': return 7, nil
+  case 'R': fallthrough
+  case 'r': return 8, nil
+  case 'Y': fallthrough
+  case 'y': return 9, nil
+  case 'B': fallthrough
+  case 'b': return 10, nil
+  case 'D': fallthrough
+  case 'd': return 11, nil
+  case 'H': fallthrough
+  case 'h': return 12, nil
+  case 'V': fallthrough
+  case 'v': return 13, nil
+  case 'N': fallthrough
+  case 'n': return 14, nil
+  default:  return 0xFF, fmt.Errorf("Code(): `%c' is not part of the alphabet", i)
+  }
+}
+
+func (IupacNucleotideAlphabet) Decode(i byte) (byte, error) {
+  switch i {
+  case  0:  return 'a', nil
+  case  1:  return 'c', nil
+  case  2:  return 'g', nil
+  case  3:  return 't', nil
+  case  4:  return 'w', nil
+  case  5:  return 's', nil
+  case  6:  return 'm', nil
+  case  7:  return 'k', nil
+  case  8:  return 'r', nil
+  case  9:  return 'y', nil
+  case 10:  return 'b', nil
+  case 11:  return 'd', nil
+  case 12:  return 'h', nil
+  case 13:  return 'v', nil
+  case 14:  return 'n', nil
+  default: return 0xFF, fmt.Errorf("Code(): `%d' is not a code of the alphabet", int(i))
+  }
+}
+
+func (IupacNucleotideAlphabet) Length() int {
+  return 15
+}
+
+func (IupacNucleotideAlphabet) ComplementCoded(i byte) (byte, error) {
+  switch i {
+  case  0:  return  3, nil
+  case  1:  return  2, nil
+  case  2:  return  1, nil
+  case  3:  return  0, nil
+  case  4:  return  4, nil
+  case  5:  return  5, nil
+  case  6:  return  7, nil
+  case  7:  return  6, nil
+  case  8:  return  9, nil
+  case  9:  return  8, nil
+  case 10:  return 13, nil
+  case 11:  return 12, nil
+  case 12:  return 11, nil
+  case 13:  return 10, nil
+  case 14:  return 14, nil
+  default: return 0xFF, fmt.Errorf("ComplementCoded(): `%d' is not a code of the alphabet", int(i))
+  }
+}
+
+func (IupacNucleotideAlphabet) Complement(i byte) (byte, error) {
+  switch i {
+  case 'A': fallthrough
+  case 'a': return 't', nil
+  case 'C': fallthrough
+  case 'c': return 'g', nil
+  case 'G': fallthrough
+  case 'g': return 'c', nil
+  case 'T': fallthrough
+  case 't': return 'a', nil
+  case 'W': fallthrough
+  case 'w': return 'w', nil
+  case 'S': fallthrough
+  case 's': return 's', nil
+  case 'M': fallthrough
+  case 'm': return 'k', nil
+  case 'K': fallthrough
+  case 'k': return 'm', nil
+  case 'R': fallthrough
+  case 'r': return 'y', nil
+  case 'Y': fallthrough
+  case 'y': return 'r', nil
+  case 'B': fallthrough
+  case 'b': return 'v', nil
+  case 'D': fallthrough
+  case 'd': return 'h', nil
+  case 'H': fallthrough
+  case 'h': return 'd', nil
+  case 'V': fallthrough
+  case 'v': return 'b', nil
+  case 'N': fallthrough
+  case 'n': return 'n', nil
+  default:  return 0xFF, fmt.Errorf("Complement(): `%c' is not part of the alphabet", i)
+  }
+}
+
+func (IupacNucleotideAlphabet) IsWildcard(i byte) bool {
+  return i == 'n' || i == 'N'
+}
